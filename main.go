2ffad16482d3a700a64af939aa305793c221ece0
doodgeMatvey <matvey.gorsky.it@gmail.com>
2022-09-21 22:15:30 +0300

    issue-8, cadence crd is added

--- main.go
+++ main.go
@@ -123,6 +123,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Redis")
 		os.Exit(1)
 	}
+	if err = (&clusterscontrollers.CadenceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Cadence")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
