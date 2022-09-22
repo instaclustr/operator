c3eabae68ce98973707da9602ecc673fb4207aae
testisnullus <danil.savchenko25@gmail.com>
2022-09-22 15:29:02 +0300

    issue-10, structs refactoring

--- apis/clusters/v2alpha1/cassandra_types.go
+++ apis/clusters/v2alpha1/cassandra_types.go
@@ -23,62 +23,25 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-type CassandraAWSSettings struct {
-	EBSEncryptionKey       string `json:"ebsEncryptionKey,omitempty"`
-	CustomVirtualNetworkID string `json:"customVirtualNetworkId,omitempty"`
-}
-
-type CassandraGCPSettings struct {
-	CustomVirtualNetworkID string `json:"customVirtualNetworkId,omitempty"`
-}
-
-type CassandraAzureSettings struct {
-	ResourceGroup string `json:"resourceGroup,omitempty"`
-}
-
 type Spark struct {
 	Version string `json:"version"`
 }
 
 type CassandraDataCentre struct {
-	ContinuousBackup               bool                      `json:"continuousBackup"`
-	ReplicationFactor              int                       `json:"replicationFactor"`
-	AWSSettings                    []*CassandraAWSSettings   `json:"awsSettings,omitempty"`
-	NumberOfNodes                  int                       `json:"numberOfNodes"`
-	PrivateIPBroadcastForDiscovery bool                      `json:"privateIpBroadcastForDiscovery"`
-	Network                        string                    `json:"network,omitempty"`
-	Tags                           []*Tag                    `json:"tags,omitempty"`
-	GCPSettings                    []*CassandraGCPSettings   `json:"gcpSettings,omitempty"`
-	ClientToClusterEncryption      bool                      `json:"clientToClusterEncryption"`
-	NodeSize                       string                    `json:"nodeSize"`
-	CloudProvider                  string                    `json:"cloudProvider"`
-	AzureSettings                  []*CassandraAzureSettings `json:"azureSettings,omitempty"`
-	Name                           string                    `json:"name"`
-	Region                         string                    `json:"region"`
-	ProviderAccountName            string                    `json:"providerAccountName,omitempty"`
-}
-
-type CassandraNode struct {
-	NodeRoles      []string `json:"nodeRoles,omitempty"`
-	Rack           string   `json:"rack,omitempty"`
-	NodeSize       string   `json:"nodeSize,omitempty"`
-	PrivateAddress string   `json:"privateAddress,omitempty"`
-	PublicAddress  string   `json:"publicAddress,omitempty"`
-	NodeID         string   `json:"nodeID,omitempty"`
-	NodeStatus     string   `json:"nodeStatus,omitempty"`
+	DataCentre                     `json:",inline"`
+	ContinuousBackup               bool `json:"continuousBackup"`
+	ReplicationFactor              int  `json:"replicationFactor"`
+	PrivateIPBroadcastForDiscovery bool `json:"privateIpBroadcastForDiscovery"`
+	ClientToClusterEncryption      bool `json:"clientToClusterEncryption"`
 }
 
 // CassandraSpec defines the desired state of Cassandra
 type CassandraSpec struct {
-	PCIComplianceMode     bool                   `json:"pciComplianceMode"`
-	TwoFactorDelete       []*TwoFactorDelete     `json:"twoFactorDelete,omitempty"`
-	DataCentres           []*CassandraDataCentre `json:"dataCentres"`
-	PrivateNetworkCluster bool                   `json:"privateNetworkCluster"`
-	CassandraVersion      string                 `json:"cassandraVersion"`
-	LuceneEnabled         bool                   `json:"luceneEnabled"`
-	PasswordAndUserAuth   bool                   `json:"passwordAndUserAuth"`
-	Name                  string                 `json:"name"`
-	SLATier               string                 `json:"slaTier"`
+	ClusterSpec         `json:",inline"`
+	DataCentres         []*CassandraDataCentre `json:"dataCentres"`
+	CassandraVersion    string                 `json:"cassandraVersion"`
+	LuceneEnabled       bool                   `json:"luceneEnabled"`
+	PasswordAndUserAuth bool                   `json:"passwordAndUserAuth"`
 }
 
 type CassandraDCStatus struct {
