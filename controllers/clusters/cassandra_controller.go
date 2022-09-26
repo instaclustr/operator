c65f4fdda20484c6bc45d4ef4f55a2f5953c6c9f
testisnullus <danil.savchenko25@gmail.com>
2022-09-26 15:30:34 +0300

    issue-12, basic Cassandra Cluster creation flow was implemented

--- controllers/clusters/cassandra_controller.go
+++ controllers/clusters/cassandra_controller.go
@@ -18,19 +18,21 @@ package clusters
 
 import (
 	"context"
-
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	clustersv2alpha1 "github.com/instaclustr/operator/apis/clusters/v2alpha1"
+	"github.com/instaclustr/operator/pkg/instaclustr"
 )
 
 // CassandraReconciler reconciles a Cassandra object
 type CassandraReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	API    *instaclustr.Client
 }
 
 //+kubebuilder:rbac:groups=clusters.instaclustr.com,resources=cassandras,verbs=get;list;watch;create;update;patch;delete
@@ -47,11 +49,59 @@ type CassandraReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
 func (r *CassandraReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	l := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var cassandraCluster clustersv2alpha1.Cassandra
+	err := r.Client.Get(ctx, req.NamespacedName, &cassandraCluster)
+	if err != nil {
+		l.Error(
+			err, "unable to fetch Cassandra Cluster",
+			"Cassandra cluster cluster spec", cassandraCluster.Spec,
+		)
+		return reconcile.Result{}, err
+	}
 
-	return ctrl.Result{}, nil
+	if cassandraCluster.Status.ClusterID == "" {
+		l.Info(
+			"Cassandra Cluster ID not found, creating Cassandra cluster",
+			"Cluster name", cassandraCluster.Spec.Name,
+			"Data centres", cassandraCluster.Spec.DataCentres,
+		)
+
+		id, err := r.API.CreateCluster(instaclustr.CassandraEndpoint, cassandraCluster.Spec)
+		if err != nil {
+			l.Error(
+				err, "cannot create Cassandra cluster",
+				"Cassandra cluster spec", cassandraCluster.Spec,
+			)
+			return reconcile.Result{}, err
+		}
+
+		currentClusterStatus, err := r.API.GetCassandraClusterStatus(id)
+		if err != nil {
+			l.Error(
+				err, "cannot get Cassandra cluster status from the Instaclustr API",
+				"Cassandra cluster spec", cassandraCluster.Spec,
+			)
+			return reconcile.Result{}, err
+		}
+
+		cassandraCluster.Status = *currentClusterStatus
+		err = r.Status().Update(context.Background(), &cassandraCluster)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+		l.Info(
+			"Cassandra resource has been created",
+			"Cluster name", cassandraCluster.Spec.Name,
+			"cluster ID", id,
+			"Kind", cassandraCluster.Kind,
+			"API Version", cassandraCluster.APIVersion,
+		)
+
+	}
+
+	return reconcile.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
