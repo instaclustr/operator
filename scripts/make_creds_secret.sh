8181096cfe7aaf7d6a8cc8e5d48b499d8160abda
testsinullus <danil.savchenko25@gmail.com>
2022-09-28 18:55:01 +0300

    issue-16, secrets creation for creds was implemented

---
+++ scripts/make_creds_secret.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+path=$(readlink -f ../.env)
+
+. $path
+export USERNAME=$(echo -n $USERNAME | base64)
+export APIKEY=$(echo -n $APIKEY | base64)
+export HOSTNAME=$(echo -n $HOSTNAME | base64)
+
+( echo "cat <<EOF >../config/manager/creds_secret.yaml";
+  cat secret.yaml;
+  echo "EOF";
+) >tmp.yaml
+. tmp.yaml
+cat ../config/manager/creds_secret.yaml
+rm tmp.yaml
