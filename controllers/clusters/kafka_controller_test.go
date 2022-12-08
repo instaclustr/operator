216fc8efaa1c15096491d5ce7a42589cfaec4c74
Stanislav Bychkov <stanislav.bychkov67@gmail.com>
2022-12-08 13:30:41 +0200

    first integration test for kafka creation

---
+++ controllers/clusters/kafka_controller_test.go
@@ -0,0 +1,76 @@
+package clusters
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/instaclustr/operator/apis/clusters/v1alpha1"
+	"github.com/instaclustr/operator/pkg/instaclustr/mock"
+	"github.com/instaclustr/operator/pkg/models"
+)
+
+var _ = Describe("Successful creation of a Kafka resource", func() {
+	Context("When setting up a Kafka CRD", func() {
+		kafkaSpec := v1alpha1.KafkaSpec{
+			Cluster: v1alpha1.Cluster{
+				Name:                  "kafkaTest",
+				Version:               "2.7.1",
+				PCICompliance:         false,
+				PrivateNetworkCluster: false,
+				SLATier:               "NON_PRODUCTION",
+				TwoFactorDelete:       nil,
+			},
+			SchemaRegistry:            nil,
+			ReplicationFactorNumber:   3,
+			PartitionsNumber:          3,
+			RestProxy:                 nil,
+			AllowDeleteTopics:         false,
+			AutoCreateTopics:          false,
+			ClientToClusterEncryption: false,
+			DataCentres: []*v1alpha1.KafkaDataCentre{{
+				NodesNumber:   3,
+				Network:       "192.168.0.0/18",
+				NodeSize:      "r5.large-500-gp2",
+				CloudProvider: "AWS_VPC",
+				Name:          "US_WEST_2_DC",
+				Region:        "US_WEST_2",
+			}},
+			DedicatedZookeeper:                nil,
+			ClientBrokerAuthWithMTLS:          false,
+			ClientAuthBrokerWithoutEncryption: false,
+			ClientAuthBrokerWithEncryption:    false,
+			KarapaceRestProxy:                 nil,
+			KarapaceSchemaRegistry:            nil,
+		}
+
+		ctx := context.Background()
+		resource := v1alpha1.Kafka{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "kafka",
+				Namespace: "default",
+				Annotations: map[string]string{
+					models.ResourceStateAnnotation: models.CreatingEvent,
+				},
+			},
+			Spec: kafkaSpec,
+		}
+
+		It("Should create a Kafka resources", func() {
+			Expect(k8sClient.Create(ctx, &resource)).Should(Succeed())
+
+			By("Sending Kafka Specification to Instaclustr API v2")
+			var kafkaCluster v1alpha1.Kafka
+			Eventually(func() bool {
+				if err := k8sClient.Get(ctx, types.NamespacedName{Name: "kafka", Namespace: "default"}, &kafkaCluster); err != nil {
+					return false
+				}
+
+				return kafkaCluster.Status.ID == mock.CreatedID
+			}).Should(BeTrue())
+		})
+	})
+})
---
+++ pkg/instaclustr/mock/client.go
@@ -0,0 +1,128 @@
+package mock
+
+import (
+	"net/http"
+
+	clusterresourcesv1alpha1 "github.com/instaclustr/operator/apis/clusterresources/v1alpha1"
+	"github.com/instaclustr/operator/apis/clusters/v1alpha1"
+	kafkamanagementv1alpha1 "github.com/instaclustr/operator/apis/kafkamanagement/v1alpha1"
+	modelsv1 "github.com/instaclustr/operator/pkg/instaclustr/api/v1/models"
+	models2 "github.com/instaclustr/operator/pkg/instaclustr/api/v2/models"
+	"github.com/instaclustr/operator/pkg/models"
+)
+
+type mockClient struct {
+	*http.Client
+}
+
+func NewInstAPI() *mockClient {
+	return &mockClient{}
+}
+
+const (
+	CreatedID = "created"
+)
+
+func (c *mockClient) CreateCluster(url string, clusterSpec any) (string, error) {
+	return CreatedID, nil
+}
+
+func (c *mockClient) DoRequest(url string, method string, data []byte) (*http.Response, error) {
+	panic("DoRequest: is not implemented")
+}
+
+func (c *mockClient) GetClusterStatus(id, clusterEndpoint string) (*v1alpha1.ClusterStatus, error) {
+	panic("GetClusterStatus: is not implemented")
+}
+func (c *mockClient) UpdateNodeSize(clusterEndpoint string, resizeRequest *models.ResizeRequest) error {
+	panic("UpdateNodeSize: is not implemented")
+}
+func (c *mockClient) GetActiveDataCentreResizeOperations(clusterID, dataCentreID string) ([]*models.DataCentreResizeOperations, error) {
+	panic("GetActiveDataCentreResizeOperations: is not implemented")
+}
+func (c *mockClient) GetClusterConfigurations(clusterEndpoint, clusterID, bundle string) (map[string]string, error) {
+	panic("GetClusterConfigurations: is not implemented")
+}
+func (c *mockClient) UpdateClusterConfiguration(clusterEndpoint, clusterID, bundle, paramName, paramValue string) error {
+	panic("UpdateClusterConfiguration: is not implemented")
+}
+func (c *mockClient) ResetClusterConfiguration(clusterEndpoint, clusterID, bundle, paramName string) error {
+	panic("ResetClusterConfiguration: is not implemented")
+}
+func (c *mockClient) UpdateDescriptionAndTwoFactorDelete(clusterEndpoint, clusterID, description string, twoFactorDelete *v1alpha1.TwoFactorDelete) error {
+	panic("UpdateDescriptionAndTwoFactorDelete: is not implemented")
+}
+func (c *mockClient) UpdateCluster(id, clusterEndpoint string, InstaDCs any) error {
+	panic("UpdateCluster: is not implemented")
+}
+func (c *mockClient) DeleteCluster(id, clusterEndpoint string) error {
+	panic("DeleteCluster: is not implemented")
+}
+func (c *mockClient) AddDataCentre(id, clusterEndpoint string, dataCentre any) error {
+	panic("AddDataCentre: is not implemented")
+}
+func (c *mockClient) GetPeeringStatus(peerID, peeringEndpoint string) (*clusterresourcesv1alpha1.PeeringStatus, error) {
+	panic("GetPeeringStatus: is not implemented")
+}
+func (c *mockClient) UpdatePeering(peerID, peeringEndpoint string, peerSpec any) error {
+	panic("UpdatePeering: is not implemented")
+}
+func (c *mockClient) DeletePeering(peerID, peeringEndpoint string) error {
+	panic("DeletePeering: is not implemented")
+}
+func (c *mockClient) CreatePeering(url string, peeringSpec any) (*clusterresourcesv1alpha1.PeeringStatus, error) {
+	panic("CreatePeering: is not implemented")
+}
+func (c *mockClient) GetFirewallRuleStatus(firewallRuleID string, firewallRuleEndpoint string) (*clusterresourcesv1alpha1.FirewallRuleStatus, error) {
+	panic("GetFirewallRuleStatus: is not implemented")
+}
+func (c *mockClient) CreateFirewallRule(url string, firewallRuleSpec any) (*clusterresourcesv1alpha1.FirewallRuleStatus, error) {
+	panic("CreateFirewallRule: is not implemented")
+}
+func (c *mockClient) DeleteFirewallRule(firewallRuleID string, firewallRuleEndpoint string) error {
+	panic("DeleteFirewallRule: is not implemented")
+}
+func (c *mockClient) GetKafkaUserStatus(kafkaUserID, kafkaUserEndpoint string) (*kafkamanagementv1alpha1.KafkaUserStatus, error) {
+	panic("GetKafkaUserStatus: is not implemented")
+}
+func (c *mockClient) CreateKafkaUser(url string, kafkaUser *models2.KafkaUserAPIv2) (*kafkamanagementv1alpha1.KafkaUserStatus, error) {
+	panic("CreateKafkaUser: is not implemented")
+}
+func (c *mockClient) UpdateKafkaUser(kafkaUserID, kafkaUserEndpoint string, kafkaUserSpec *models2.KafkaUserAPIv2) error {
+	panic("UpdateKafkaUser: is not implemented")
+}
+func (c *mockClient) DeleteKafkaUser(kafkaUserID, kafkaUserEndpoint string) error {
+	panic("DeleteKafkaUser: is not implemented")
+}
+func (c *mockClient) CreateKafkaTopic(url string, topic *kafkamanagementv1alpha1.Topic) error {
+	panic("CreateKafkaTopic: is not implemented")
+}
+func (c *mockClient) DeleteKafkaTopic(url, id string) error {
+	panic("DeleteKafkaTopic: is not implemented")
+}
+func (c *mockClient) UpdateKafkaTopic(url string, topic *kafkamanagementv1alpha1.Topic) error {
+	panic("UpdateKafkaTopic: is not implemented")
+}
+func (c *mockClient) CreateKafkaMirror(url string, m *kafkamanagementv1alpha1.Mirror) error {
+	panic("CreateKafkaMirror: is not implemented")
+}
+func (c *mockClient) DeleteKafkaMirror(url, id string) error {
+	panic("DeleteKafkaMirror: is not implemented")
+}
+func (c *mockClient) UpdateKafkaMirror(url string, m *kafkamanagementv1alpha1.Mirror) error {
+	panic("UpdateKafkaMirror: is not implemented")
+}
+func (c *mockClient) GetClusterBackups(endpoint, clusterID string) (*models.ClusterBackup, error) {
+	panic("GetClusterBackups: is not implemented")
+}
+func (c *mockClient) TriggerClusterBackup(url, clusterID string) error {
+	panic("TriggerClusterBackup: is not implemented")
+}
+
+func (c *mockClient) CreateNodeReload(bundle, nodeID string, nr *modelsv1.NodeReload) error {
+	panic("implement me")
+}
+
+func (c *mockClient) GetNodeReloadStatus(bundle, nodeID string) (*modelsv1.NodeReloadStatusAPIv1, error) {
+	panic("implement me")
+}
