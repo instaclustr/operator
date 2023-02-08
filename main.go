07ddae3bdda24d9eb0c57b0f7653d315d9a83a92
doodgeMatvey <matvey.gorsky.it@gmail.com>
2023-02-08 17:46:29 +0200

    issue-89, validation for Cluster Network Firewall Rule was implemented

--- main.go
+++ main.go
@@ -314,6 +314,10 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "Kafka")
 		os.Exit(1)
 	}
+	if err = (&clusterresourcesv1alpha1.ClusterNetworkFirewallRule{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "ClusterNetworkFirewallRule")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
