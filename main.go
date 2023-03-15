cf4822057c3925084895ee2087917034699851a7
Rostislav Porohnya <rostislav.porohnya@gmail.com>
2023-03-15 13:19:53 +0200

    issue-120, k8s resource events for Maintenance events resource is implemented

--- main.go
+++ main.go
@@ -297,10 +297,11 @@ func main() {
 		os.Exit(1)
 	}
 	if err = (&clusterresourcescontrollers.MaintenanceEventsReconciler{
-		Client:    mgr.GetClient(),
-		Scheme:    mgr.GetScheme(),
-		API:       instaClient,
-		Scheduler: s,
+		Client:        mgr.GetClient(),
+		Scheme:        mgr.GetScheme(),
+		API:           instaClient,
+		Scheduler:     s,
+		EventRecorder: eventRecorder,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "MaintenanceEvents")
 		os.Exit(1)
