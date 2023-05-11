20583424fb964ad7afd890133d5d9f83dd13640f
doodgeMatvey <matvey.gorsky.it@gmail.com>
2023-05-11 17:00:12 +0300

    issue-131-fix, api groups was changed for services and endpoints

--- controllers/clusters/cassandra_controller.go
+++ controllers/clusters/cassandra_controller.go
@@ -60,8 +60,8 @@ type CassandraReconciler struct {
 //+kubebuilder:rbac:groups=clusters.instaclustr.com,resources=cassandras/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=clusters.instaclustr.com,resources=cassandras/finalizers,verbs=update
 //+kubebuilder:rbac:groups="",resources=events,verbs=create;patch
-//+kubebuilder:rbac:groups=*,resources=services,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=*,resources=endpoints,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=endpoints,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
